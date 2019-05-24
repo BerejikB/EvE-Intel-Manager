3b8906152d6d0d524afc7ad260238a7f97d16844
Brendan Berejikian <bezoric1@gmail.com>
2019-05-24 12:27:59 -0400

    gdgd

--- Eve Intel Manager/Eve Intel Manager/Entities/ReportsDbCon.cs
+++ Eve Intel Manager/Eve Intel Manager/Entities/ReportsDbCon.cs
@@ -5,15 +5,17 @@ using System.Threading.Tasks;
 using Eve_Intel_Manager;
 using EVEStandard;
 using Eve_Intel_Manager.Views.Secure;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Eve_Intel_Manager.Entities
 {
     public class EIMReportsDbContext : DbContext
     {
-        public EIMReportsDbContext()
-         : base("Server=tcp:eveintelmanager20190521111543dbserver.database.windows.net,1433;Initial Catalog=EveIntelManager20190521111543_db;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
-        {}                
-       public DbSet<Reports> Report { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:eveintelmanager20190521111543dbserver.database.windows.net,1433;Initial Catalog=EveIntelManager20190521111543_db;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+        
+        public DbSet<Reports> Report { get; set; }
     }
 }
