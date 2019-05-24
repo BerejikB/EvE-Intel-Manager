acfe2a12d00c832ebe372f4a3f031d87e484785a
Brendan Berejikian <bezoric1@gmail.com>
2019-05-24 13:24:58 -0400

    Still working on dashboard

--- Eve Intel Manager/Eve Intel Manager/Entities/Reports.cs
+++ Eve Intel Manager/Eve Intel Manager/Entities/Reports.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Eve_Intel_Manager;
+using EVEStandard;
+using Eve_Intel_Manager.Views.Secure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
-namespace Eve_Intel_Manager.Views.Secure
-{
+
+namespace Eve_Intel_Manager.Entities
+{ 
     public class Reports
     {
         public static DateTime UtcNow { get; }
